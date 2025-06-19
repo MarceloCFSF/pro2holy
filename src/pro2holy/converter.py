7ccb8caa5170999b9c91d12498f39637d645d69d
MarceloCFSF <marcelo.cfsf@gmail.com>
2025-06-19 20:10:12 -0300

    feat: add possibility to convert all folder files

--- src/pro2holy/converter.py
+++ src/pro2holy/converter.py
@@ -4,8 +4,12 @@ from bs4 import BeautifulSoup
 
 
 def converter(input_file: str, output_folder: str):
-    path = Path(input_file)
-    filename = path.stem
+    input_path = Path(input_file)
+    filename = input_path.stem
+
+    output_path = Path(output_folder)
+
+    # assert output_path.is_dir(), "O caminho de saída não é um diretório"
 
     with open(input_file, "r", encoding="utf-8") as file:
         soup = BeautifulSoup(file, "xml")
@@ -21,3 +25,10 @@ def converter(input_file: str, output_folder: str):
 
     with open(f"{output_folder}/{filename}.txt", "w", encoding="utf-8") as f:
         f.write(content)
+
+def folder_converter(input_folder: str, output_folder: str):
+    directory = Path(input_folder)
+    pro6_files = list(directory.glob("*.pro6"))
+
+    for file in pro6_files:
+        converter(f"{input_folder}/{file.name}", output_folder)
--- src/pro2holy/main.py
+++ src/pro2holy/main.py
@@ -1,18 +1,34 @@
 import argparse
-from pro2holy.converter import converter
+from pathlib import Path
+from pro2holy.converter import converter, folder_converter
 import os
+import sys
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Conversor de arquivo ProPresenter para o formato Holyrics."
-    )
-    parser.add_argument("input", help="Caminho para o arquivo .pro6")
-    parser.add_argument("output_dir", help="Pasta onde o arquivo Holyrics .txt será salvo")
+    try:
+        parser = argparse.ArgumentParser(
+            description="Conversor de arquivo ProPresenter para o formato Holyrics."
+        )
+        parser.add_argument("input", help="Caminho para o arquivo .pro6")
+        parser.add_argument("output_dir", help="Pasta onde o arquivo Holyrics .txt será salvo")
 
-    args = parser.parse_args()
+        args = parser.parse_args()
 
-    os.makedirs(args.output_dir, exist_ok=True)
-    output = converter(args.input, args.output_dir)
+        input = Path(args.input)
+        output_dir = Path(args.output_dir)
 
-    print(f"Arquivo convertido e salvo em: {output}")
+        assert input.exists(), "O caminho de entrada não existe"
+        assert not output_dir.is_file(), "O caminho de saida digitado é um arquivo"
+
+        os.makedirs(args.output_dir, exist_ok=True)
+
+        if (input.is_dir()):
+            folder_converter(args.input, args.output_dir)
+        else:
+            converter(args.input, args.output_dir)
+
+        print(f"Arquivo convertido e salvo em: {output_dir.resolve()}")
+    except AssertionError as e:
+        print(f"[ERROR] {e}", file=sys.stderr)
+        sys.exit(1)
